a16306d1dae9dd46acee21aade66d215c36a2ee6
Jayesh0205 <jayeshgujar9051@gmail.com>
2026-05-08 00:27:58 +0530

    Assignment 1

---
+++ assignment1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Student Resume</title>
+</head>
+
+<body>
+
+    <h1>Jayesh Gujar</h1>
+
+    <p>
+        Hello! I am a Computer Engineering student.
+        I am learning Full Stack Development including HTML, CSS, and PHP.
+        I like coding and creating simple websites.
+    </p>
+
+    <h2>Education</h2>
+
+    <table border="1">
+
+        <tr>
+            <th>Course</th>
+            <th>College/School</th>
+            <th>Year</th>
+        </tr>
+
+        <tr>
+            <td>B.Tech Computer Engineering</td>
+            <td>DBATU University</td>
+            <td>2026</td>
+        </tr>
+
+        <tr>
+            <td>HSC</td>
+            <td>A.G.R.G.M.V & Junior College</td>
+            <td>2022</td>
+        </tr>
+
+        <tr>
+            <td>SSC</td>
+            <td>A.G.R.G.M.V. Shendurni</td>
+            <td>2020</td>
+        </tr>
+
+    </table>
+
+    <h2>Skills</h2>
+
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>PHP</li>
+        <li>C Programming</li>
+    </ul>
+
+    <h2>Contact Form</h2>
+
+    <form>
+
+        <label>Name:</label><br>
+        <input type="text"><br><br>
+
+        <label>Email:</label><br>
+        <input type="email"><br><br>
+
+        <label>Message:</label><br>
+        <textarea rows="5" cols="30"></textarea><br><br>
+
+        <input type="submit" value="Submit">
+
+    </form>
+
+</body>
+
+</html>
