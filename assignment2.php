855a1f16756bbd5e64d59fe487cb4a14e5a6dc15
Jayesh0205 <jayeshgujar9051@gmail.com>
2026-05-08 00:28:31 +0530

    Assignment 2

---
+++ assignment2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Me</title>
+
+    <style>
+
+        body {
+            background-color: lightblue;
+            text-align: center;
+            margin: 20px;
+            padding: 20px;
+        }
+
+        h1 {
+            color: darkblue;
+        }
+
+        p {
+            color: black;
+            margin: 15px;
+            padding: 10px;
+        }
+
+        img {
+            border: 3px solid black;
+            margin: 10px;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <h1>About Me</h1>
+
+    <img src="spider-man-and-gwen-stacy-4k-n1v7uo58axknt9mz.jpg" height="200" width="200">
+
+    <p>
+        Hello! My name is Jayesh Gujar. I am currently studying Computer Engineering.
+    </p>
+
+    <p>
+        My hobbies are playing games, listening to music, and exploring new things
+        related to computers and technology.
+    </p>
+
+    <p>
+        I am learning HTML, CSS, and PHP in a full stack development course.
+    </p>
+
+</body>
+</html>
