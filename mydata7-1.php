eabb9f20f6821ac052a33d3244e8a0b11d6f876e
Jayesh0205 <jayeshgujar9051@gmail.com>
2026-05-08 18:11:13 +0530

    Assignment 7

---
+++ mydata7-1.php
@@ -0,0 +1,18 @@
+<?php
+$student = [
+    [
+        "id" => 1,
+        "name" => "Bhavesh"
+    ],
+    [
+        "id" => 2,
+        "name" => "Shubham"
+    ],
+    [
+        "id" => 3,
+        "name" => "Khushal"
+    ]
+];
+
+print_r(json_encode($student));
+?>
